f31cf24788d2c5d8208da005e8b96e3ec3758507
Rajitha, Mareddy <Mareddy.Rajitha@transunion.com>
2020-04-11 14:22:45 +0530

    added aprogram of concate string

---
+++ src/Strings_Practice/StringConcate.java
@@ -0,0 +1,11 @@
+package Strings_Practice;
+
+public class StringConcate {
+
+	public static void main(String[] args) {
+		String s = "abc";
+		String s1 = "cde";
+		System.out.println(s.concat(s1));
+	}
+
+}
